a5623f8ca5947d4cd59e7d3f7874cc7814f5bc3a
Zaryab Ahmed <za366@drexel.edu>
2025-09-03 17:04:27 -0400

    style primary header (first line of header)

--- src/routes/edit/[articleId]/Header.svelte
+++ src/routes/edit/[articleId]/Header.svelte
@@ -14,6 +14,43 @@ let stageName = "Authoring";
 let title = $state("Arson and antisemitic graffiti heighten fears of hate crime on Drexel campus");
 </script>
 <style>
+    header {
+        position: sticky;
+        top: 0;
+    }
+    #primary-header {
+        display: flex;
+        gap: 0.5rem;
+        align-items: center;
+        margin: 1rem;
+        * {
+            background: inherit;
+            font-family: serif;
+        }
+        button {
+            font-size: 1rem;
+            color: var(--primary-800);
+        }
+    }
+    #desk-stage {
+        font-size: 0.875rem;
+        font-weight: 700;
+        background: var(--secondary-3);
+        padding: 0.25rem 0.75rem;
+        margin: 0;
+        border-radius: 0.25rem;
+    }
+    #headline {
+        font-size: 1.5rem;
+        line-height: 2rem;
+        flex: 1 1 0%;
+    }
+    #headline:hover {
+        border-bottom: 2px solid var(--neutral-primary-2);
+    }
+    #headline:focus-visible {
+        border-bottom: 2px solid var(--primary-800);
+    }
 </style>
 
 <header>

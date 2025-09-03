2c576719e54438cc547f7b0523689b8b88d705b2
Zaryab Ahmed <za366@drexel.edu>
2025-09-03 17:01:37 -0400

    move default pages to /playground, with additional page for opening the editor

--- src/routes/playground/+layout.svelte
+++ src/routes/playground/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import Header from './Header.svelte';
-	import '../app.css';
+	import '$lib/styles/playground.css';
 
 	let { children } = $props();
 </script>
---
+++ src/routes/playground/edit/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import { goto } from "$app/navigation";
+    let articleId: string = $state("");
+</script>
+
+<label for="articleId">Enter article ID: </label>
+<input id="articleId" bind:value={articleId} />
+<button onclick={() => goto(`/edit/${articleId}`)}>Edit</button>

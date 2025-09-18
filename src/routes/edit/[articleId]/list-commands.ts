b4cb3f74a28d866e2fe0e574ae6901d40c979cc7
Zaryab Ahmed <za366@drexel.edu>
2025-09-18 18:49:09 -0400

    implemented input rules and keymaps for intuitively using lists

---
+++ src/routes/edit/[articleId]/list-commands.ts
@@ -0,0 +1,14 @@
+import { EditorState } from "prosemirror-state";
+import { sinkListItem } from "prosemirror-schema-list";
+import { schema } from "./schema";
+
+export function tabListItem(state: EditorState, dispatch?: (tr: Transaction) => any): boolean {
+    const fromRes = state.selection.$from;
+    const nodeType = fromRes.node(fromRes.depth-1).type;
+    // If selection start position is inside a list item and at the beginning of said list item
+    if (nodeType == schema.nodes.list_item && fromRes.parentOffset == 0) {
+        // Then try to sink the list item into a nested list of the appropriate type
+        console.log("yooo wassap");
+        return sinkListItem(schema.nodes.list_item)(state, dispatch);
+    } else return true;
+}

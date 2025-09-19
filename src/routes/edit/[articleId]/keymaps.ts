4dccf669d17956e0107e1fef01742dc02831ea20
Zaryab Ahmed <za366@drexel.edu>
2025-09-18 20:25:40 -0400

    add keymaps for headings

--- src/routes/edit/[articleId]/keymaps.ts
+++ src/routes/edit/[articleId]/keymaps.ts
@@ -1,13 +1,20 @@
 import { undoInputRule } from "prosemirror-inputrules";
 import { splitListItem, liftListItem, sinkListItem } from "prosemirror-schema-list";
+import { setBlockType, toggleMark } from "prosemirror-commands";
 import { undo, redo } from "prosemirror-history";
 import { schema } from "./schema";
 import { tabListItem } from "./list-commands";
+
+const setHeading = (level: number) => setBlockType(schema.nodes.heading, { level });
 
 export const keymaps = {
     "Backspace": undoInputRule,
     "Enter": splitListItem(schema.nodes.list_item),
     "Tab": tabListItem,
     "Shift-Tab": liftListItem(schema.nodes.list_item),
-    "Mod-z": undo, "Mod-Shift-z": redo, "Mod-y": redo
+    "Mod-z": undo, "Mod-Shift-z": redo, "Mod-y": redo,
+    "Mod-Alt-0": setBlockType(schema.nodes.paragraph),
+    "Mod-Alt-1": setHeading(1), "Mod-Alt-2": setHeading(2),
+    "Mod-Alt-3": setHeading(3), "Mod-Alt-4": setHeading(4),
+    "Mod-Alt-5": setHeading(5), "Mod-Alt-6": setHeading(6)
 }; 

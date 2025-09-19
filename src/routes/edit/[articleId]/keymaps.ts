903cb1a13945ce88fca7ba1c9fa26eb31761f0a4
Zaryab Ahmed <za366@drexel.edu>
2025-09-18 20:08:18 -0400

    add keymaps for undo and redo

--- src/routes/edit/[articleId]/keymaps.ts
+++ src/routes/edit/[articleId]/keymaps.ts
@@ -1,5 +1,6 @@
 import { undoInputRule } from "prosemirror-inputrules";
 import { splitListItem, liftListItem, sinkListItem } from "prosemirror-schema-list";
+import { undo, redo } from "prosemirror-history";
 import { schema } from "./schema";
 import { tabListItem } from "./list-commands";
 
@@ -7,5 +8,6 @@ export const keymaps = {
     "Backspace": undoInputRule,
     "Enter": splitListItem(schema.nodes.list_item),
     "Tab": tabListItem,
-    "Shift-Tab": liftListItem(schema.nodes.list_item)
+    "Shift-Tab": liftListItem(schema.nodes.list_item),
+    "Mod-z": undo, "Mod-Shift-z": redo, "Mod-y": redo
 }; 

17fde791ce61c2d9cb43faabccb7d9a65047e874
Zaryab Ahmed <za366@drexel.edu>
2025-09-28 06:41:37 -0400

    show loading icon when saving

--- src/routes/edit/[articleId]/Header.svelte
+++ src/routes/edit/[articleId]/Header.svelte
@@ -2,11 +2,14 @@
 import { enhance } from "$app/forms";
 import Toolbar from "./Toolbar.svelte";
 import MetaFields from "./MetaFields.svelte";
+import LoaderIcon from "$lib/images/loader-2-line.svelte";
 
 const { editorState, dispatch, focusEditor, headline, etag } = $props();
 
 let deskName = "Opinion";
 let stageName = "Authoring";
+
+let saving = $state(false);
 </script>
 
 <style>
@@ -31,7 +34,7 @@ let stageName = "Authoring";
             font-size: 1rem;
             color: var(--primary-800);
         }
-        button:hover, input[type=submit]:hover {
+        button:hover, input[type=submit]:hover:not([disabled]) {
             color: var(--secondary-4);
             cursor: pointer;
         }
@@ -57,20 +60,48 @@ let stageName = "Authoring";
         border-bottom: 2px solid var(--primary-800);
         outline: none;
     }
+    #submit-button {
+        position: relative;
+        :global(svg) {
+            position: absolute;
+            left: 0;
+            right: 0;
+            margin-inline: auto;
+            height: 100%;
+            width: 100%;
+            fill: var(--primary-800);
+            animation: spin 2000ms infinite linear;
+        }
+    }
+    @keyframes spin {
+        from {
+            transform:rotate(0deg);
+        }
+        to {
+            transform:rotate(360deg);
+        }
+    }
 </style>
 
 <header>
     <form method="post" use:enhance={({ formData }) => {
             formData.append("body_html", document.querySelector(".ProseMirror")?.innerHTML.replace(/"/g, '\\"') || "");
             formData.append("_etag", etag);
-            return async ({ update }) => update({ reset: false });
+            saving = true;
+            return async ({ update }) => {
+                update({ reset: false });
+                saving = false;
+            };
         }}>
         <div id="primary-header">
             <!-- TODO: when highlighted, displays "headline" to indicate you're editing the headline field -->
             <p id="desk-stage">{deskName.toUpperCase()} / {stageName.toUpperCase()}</p>
             <input id="headline" name="headline" value={headline} autocomplete="off" />
             <button>CLOSE</button>
-            <input type="submit" value="SAVE" />
+            <span id="submit-button">
+                <input type="submit" value="SAVE" style:color={saving ? "var(--neutral-primary-1)" : undefined} disabled={saving} />
+                {#if saving}<LoaderIcon />{/if}
+            </span>
             <button>SEND</button>
         </div>
         <Toolbar {editorState} {dispatch} {focusEditor} />

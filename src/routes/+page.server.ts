7d0b43d9137881284e5f89fc5c739b3e900a6336
Zaryab Ahmed <za366@drexel.edu>
2025-10-05 20:18:16 -0400

    add root page which displays all content items

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,23 @@
+import type { Cookies } from "@sveltejs/kit";
+import dotenv from "dotenv";
+import { getSessionToken, redirectToLogin, AuthenticationError, fetchJsonAuthenticated } from "$lib/server/auth";
+
+dotenv.config();
+const SD_API_URL = process.env.VITE_SD_API_URL;
+
+export async function load({ params, cookies, url }: { params: { articleId: string }, cookies: Cookies, url: URL }) {
+    try {
+        let session_token = getSessionToken(cookies);
+
+        let archive = await fetchJsonAuthenticated(session_token, `${SD_API_URL}/archive`);
+        // let desks = await fetchJsonAuthenticated(session_token, `${SD_API_URL}/desks`);
+        // let stage = await fetchJsonAuthenticated(session_token, `${SD_API_URL}/stages/${content.task.stage}`);
+
+        return {
+            archive//, desks//, stage
+        };
+    } catch (e) { // If authentication fails, redirect to login page
+        if (e instanceof AuthenticationError) redirectToLogin(url.origin+url.pathname);
+        else throw e;
+    }
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+import "$lib/styles/app.css";
+import ContentItem from "./ContentItem.svelte";
+
+const { data } = $props();
+</script>
+
+<div style:width="50%">
+{#each data.archive._items as item}
+<ContentItem {item}/>
+{/each}
+</div>

29d55366cf0afc027313a4d96fe14c4523799ffc
Zaryab Ahmed <za366@drexel.edu>
2026-01-20 04:57:45 -0500

    use whole json object of vocab items in multiselects instead of just the code

--- src/routes/edit/[articleId]/MetaFields.svelte
+++ src/routes/edit/[articleId]/MetaFields.svelte
@@ -11,8 +11,8 @@
     const headerFieldIds = [...metaFields.keys().filter((f) => metaFields.get(f)?.section == "header")].sort((a, b) => metaFields.get(a).order - metaFields.get(b).order );
     const contentFieldIds = [...metaFields.keys().filter((f) => metaFields.get(f)?.section == "content")].sort((a, b) => metaFields.get(a).order - metaFields.get(b).order );
 
-    const multiOptions = (field: string) => vocabs.get(fieldVocabs[field])?.items.map((i: any) => ({ name: i.name, value: i.qcode }));
-    const multiValues = (field: string) => Object.getOwnPropertyDescriptor(item, field)?.value?.map((i: any) => i.qcode);
+    const multiOptions = (field: string) => vocabs.get(fieldVocabs[field])?.items.map((i: any) => ({ name: i.name, value: JSON.stringify(i) }));
+    const multiValues = (field: string) => Object.getOwnPropertyDescriptor(item, field)?.value?.map((i: any) => JSON.stringify(i));
 
     let open = $state(false);
 

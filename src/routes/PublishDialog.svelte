62aab31f37e3b2a2319f6536dd63d1291aeb6929
Zaryab Ahmed <za366@drexel.edu>
2026-01-30 22:53:39 -0500

    add x to close publish dialog

--- src/routes/PublishDialog.svelte
+++ src/routes/PublishDialog.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 import { enhance } from "$app/forms";
+import XIcon from "$lib/images/x.svelte";
 
 let { publishing = $bindable(), subscribers } = $props();
 
@@ -29,12 +30,32 @@ form {
     border-radius: 10px;
     border: 1px solid black;
     padding: 1rem 2rem;
+    position: relative;
     display: flex;
     flex-direction: column;
     * {
         margin: 0.5rem 0 ;
         text-align: center;
     }
+}
+#close-publishing {
+    position: absolute;
+    top: 1rem;
+    right: 1rem;
+    border-radius: 9999px;
+    background: none;
+    margin: 0;
+    padding: 0;
+    :global(svg) {
+        width: 1.5rem;
+        height: 1.5rem;
+        vertical-align: bottom;
+    }
+}
+#close-publishing:hover {
+    color: red;
+    cursor: pointer;
+    background: #00000022;
 }
 </style>
 
@@ -49,7 +70,7 @@ form {
             };
         }}>
             <h3>PUBLISH</h3>
-            <button id="close-publishing" onclick={() => dialog.close()}>close</button>
+            <button id="close-publishing" type="button" onclick={() => dialog.close()}><XIcon /></button>
             <label for="subscribers">Target Subscribers:</label>
             <select name="subscribers">
             {#each subscribers as subscriber}

1daea74e663a28f9b63677f4a866ee577ec9ae5b
Zaryab Ahmed <za366@drexel.edu>
2026-02-05 14:56:07 -0500

    forgot to add notifications.ts changes for new createNotification fn in last commit

--- src/routes/notifications.ts
+++ src/routes/notifications.ts
@@ -5,3 +5,22 @@ export interface NotificationItem {
     timeoutId?: ReturnType<typeof setTimeout>;
     visible: boolean;
 }
+
+export function createNotification(message: string, notifications: NotificationItem[], nextNotifId: number) {
+    let newNotif: NotificationItem = {
+        id: nextNotifId,
+        message,
+        color: "red",
+        visible: true
+    };
+    newNotif.timeoutId = setTimeout(() => {
+        const notif = notifications.find((n) => n.id == newNotif.id);
+        if (notif) {
+            if (notif.timeoutId) clearTimeout(notif.timeoutId);
+            notif.visible = false;
+            setTimeout(() => notifications = notifications.filter((n) => n.id !== notif?.id), 300);
+        }
+    }, 8000);
+    notifications.push(newNotif);
+    nextNotifId += 1;
+}
